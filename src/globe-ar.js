1e689f3ba59f974fc19eade49883ff5466974f0e
vasco <vastur@gmail.com>
2019-12-22 01:13:58 -0800

    Add src code

---
+++ src/globe-ar.js
@@ -0,0 +1,291 @@
+import 'aframe-globe-component';
+
+//import * as a from 'ar.js/aframe/build/aframe-ar.js';
+
+import Kapsule from 'kapsule';
+import accessorFn from 'accessor-fn';
+
+//
+
+const GLOBE_RADIUS = 200; // in GL units
+
+export default Kapsule({
+
+  props: {
+    width: {},
+    height: {},
+    yOffset: { default: 1.5 }, // marker size units
+    globeScale: { default: 1 }, // globe radius units per marker width
+
+    globeImageUrl: {},
+    bumpImageUrl: {},
+    showAtmosphere: { default: true },
+    showGraticules: { default: false },
+    pointsData: { default: [] },
+    pointLabel: { default: 'name' },
+    pointDesc: { default: 'desc' },
+    pointLat: { default: 'lat' },
+    pointLng: { default: 'lng' },
+    pointColor: { default: () => '#ffffaa' },
+    pointAltitude: { default: 0.1 }, // in units of globe radius
+    pointRadius: { default: 0.25 }, // in deg
+    pointResolution: { default: 12 }, // how many slice segments in the cylinder's circumference
+    pointsMerge: { default: false }, // boolean. Whether to merge all points into a single mesh for rendering performance
+    pointsTransitionDuration: { default: 1000 }, // ms
+    arcsData: { default: [] },
+    arcLabel: { default: 'name' },
+    arcDesc: { default: 'desc' },
+    arcStartLat: { default: 'startLat' },
+    arcStartLng: { default: 'startLng' },
+    arcEndLat: { default: 'endLat' },
+    arcEndLng: { default: 'endLng' },
+    arcColor: { default: () => '#ffffaa' },
+    arcAltitude: {}, // in units of globe radius
+    arcAltitudeAutoScale: { default: 0.5 }, // scale altitude proportional to great-arc distance between the two points
+    arcStroke: {}, // in deg
+    arcCurveResolution: { default: 64 }, // how many straight segments in the curve
+    arcCircularResolution: { default: 6 }, // how many slice segments in the tube's circumference
+    arcDashLength: { default: 1 }, // in units of line length
+    arcDashGap: { default: 0 },
+    arcDashInitialGap: { default: 0 },
+    arcDashAnimateTime: { default: 0 }, // ms
+    arcsTransitionDuration: { default: 1000 }, // ms
+    polygonsData: { default: [] },
+    polygonLabel: { default: 'name' },
+    polygonDesc: { default: 'desc' },
+    polygonGeoJsonGeometry: { default: 'geometry' },
+    polygonSideColor: { default: () => '#ffffaa' },
+    polygonCapColor: { default: () => '#ffffaa' },
+    polygonStrokeColor: {},
+    polygonAltitude: { default: 0.1 }, // in units of globe radius
+    polygonsTransitionDuration: { default: 1000 }, // ms
+    pathsData: { default: [] },
+    pathLabel: { default: 'name' },
+    pathDesc: { default: 'desc' },
+    pathPoints: { default: pnts => pnts },
+    pathPointLat: { default: arr => arr[0] },
+    pathPointLng: { default: arr => arr[1] },
+    pathPointAlt: { default: 1e-3 },
+    pathResolution: { default: 2 }, // in deg
+    pathColor: { default: () => '#ffffaa' },
+    pathStroke: {}, // in deg
+    pathDashLength: { default: 1 }, // in units of line length
+    pathDashGap: { default: 0 },
+    pathDashInitialGap: { default: 0 },
+    pathDashAnimateTime: { default: 0 }, // ms
+    pathTransitionDuration: { default: 1000 }, // ms
+    hexBinPointsData: { default: [] },
+    hexLabel: { default: 'name' },
+    hexDesc: { default: 'desc' },
+    hexBinPointLat: { default: 'lat' },
+    hexBinPointLng: { default: 'lng' },
+    hexBinPointWeight: { default: 1 },
+    hexBinResolution: { default: 4 }, // 0-15. Level 0 partitions the earth in 122 (mostly) hexagonal cells. Each subsequent level sub-divides the previous in roughly 7 hexagons.
+    hexMargin: { default: 0.2 }, // in fraction of diameter
+    hexTopColor: { default: () => '#ffffaa' },
+    hexSideColor: { default: () => '#ffffaa' },
+    hexAltitude: { default: ({ sumWeight }) => sumWeight * 0.01 }, // in units of globe radius
+    hexBinMerge: { default: false }, // boolean. Whether to merge all hex geometries into a single mesh for rendering performance
+    hexTransitionDuration: { default: 1000 }, // ms
+    hexPolygonsData: { default: [] },
+    hexPolygonLabel: { default: 'name' },
+    hexPolygonDesc: { default: 'desc' },
+    hexPolygonGeoJsonGeometry: { default: 'geometry' },
+    hexPolygonColor: { default: () => '#ffffaa' },
+    hexPolygonAltitude: { default: 0.001 }, // in units of globe radius
+    hexPolygonResolution: { default: 3 }, // 0-15. Level 0 partitions the earth in 122 (mostly) hexagonal cells. Each subsequent level sub-divides the previous in roughly 7 hexagons.
+    hexPolygonMargin: { default: 0.2 }, // in fraction of hex diameter
+    hexPolygonsTransitionDuration: { default: 0 }, // ms
+    labelsData: { default: [] },
+    labelLabel: { default: 'name' },
+    labelDesc: { default: 'desc' },
+    labelLat: { default: 'lat' },
+    labelLng: { default: 'lng' },
+    labelAltitude: { default: 0 }, // in units of globe radius
+    labelText: { default: 'text' },
+    labelSize: { default: 0.5 }, // text height in deg
+    labelTypeFace: {},
+    labelColor: { default: () => 'lightgrey' },
+    labelRotation: { default: 0 }, // clockwise degrees, relative to the latitute parallel plane
+    labelResolution: { default: 3 }, // how many segments in the text's curves
+    labelIncludeDot: { default: true },
+    labelDotRadius: { default: 0.1 }, // in deg
+    labelDotOrientation: { default: () => 'bottom' }, // right, top, bottom
+    labelsTransitionDuration: { default: 1000 }, // ms
+    customLayerData: { default: [] },
+    customLayerLabel: { default: 'name' },
+    customLayerDesc: { default: 'desc' },
+    customThreeObject: {},
+    customThreeObjectUpdate: { triggerUpdate: false }
+  },
+
+  methods: {
+    // pass-through methods
+    ...Object.assign({}, ...[
+      'getCoords',
+      'toGeoCoords'
+    ].map(method => ({
+      [method]: function (state, ...args) {
+        const aframeComp = state.globe.components.globe;
+        const returnVal = aframeComp[method](...args);
+
+        return returnVal === aframeComp
+          ? this // chain based on this object, not the inner aframe component
+          : returnVal;
+      }
+    }))),
+    _destructor: function() {
+      this.pointsData([]);
+      this.arcsData([]);
+      this.polygonsData([]);
+      this.pathsData([]);
+      this.hexBinPointsData([]);
+      this.hexPolygonsData([]);
+      this.labelsData([]);
+      this.customLayerData([]);
+    }
+  },
+
+  init(domNode, state, { markerAttrs = { preset: 'hiro' }} = {}) {
+    // Wipe DOM
+    domNode.innerHTML = '';
+
+    state.container = document.createElement('div');
+    domNode.appendChild(state.container);
+
+    // Create scene
+    const scene = document.createElement('a-scene');
+    scene.setAttribute('embedded', '');
+    scene.setAttribute('arjs', 'debugUIEnabled: false;');
+
+    const arMarker = document.createElement('a-marker');
+    // add marker attributes
+    Object.entries(markerAttrs).forEach(([attr, val]) => arMarker.setAttribute(attr, val));
+    scene.appendChild(arMarker);
+
+    // Add globe entity
+    state.globe = document.createElement('a-entity')
+    state.globe.setAttribute('globe', null);
+    arMarker.appendChild(state.globe);
+
+    // link label/desc props
+    state.globe.setAttribute('globe', {
+      label: ({type, data}) => accessorFn(state[`${type}Label`])(data),
+      desc: ({type, data}) => accessorFn(state[`${type}Desc`])(data)
+    });
+
+    const cameraEntity = document.createElement('a-entity');
+    cameraEntity.setAttribute('camera', '');
+    scene.appendChild(cameraEntity);
+
+    // attach scene
+    state.container.appendChild(scene);
+    //domNode.appendChild(scene);
+  },
+
+  update(state, changedProps) {
+    changedProps.hasOwnProperty('width') && state.width && (state.container.style.width = state.width);
+    changedProps.hasOwnProperty('height') && state.height && (state.container.style.height = state.height);
+
+    changedProps.hasOwnProperty('glScale') &&
+      state.globe.setAttribute('scale', [...new Array(3)].map(() => 1 / (state.glScale * GLOBE_RADIUS)).join(' '));
+
+    changedProps.hasOwnProperty('yOffset') &&
+    state.globe.setAttribute('position', `0 ${state.yOffset} 0`);
+
+    const passThroughProps = [
+      'globeImageUrl',
+      'bumpImageUrl',
+      'showAtmosphere',
+      'showGraticules',
+      'pointsData',
+      'pointLat',
+      'pointLng',
+      'pointColor',
+      'pointAltitude',
+      'pointRadius',
+      'pointResolution',
+      'pointsMerge',
+      'pointsTransitionDuration',
+      'arcsData',
+      'arcStartLat',
+      'arcStartLng',
+      'arcEndLat',
+      'arcEndLng',
+      'arcColor',
+      'arcAltitude',
+      'arcAltitudeAutoScale',
+      'arcStroke',
+      'arcCurveResolution',
+      'arcCircularResolution',
+      'arcDashLength',
+      'arcDashGap',
+      'arcDashInitialGap',
+      'arcDashAnimateTime',
+      'arcsTransitionDuration',
+      'polygonsData',
+      'polygonGeoJsonGeometry',
+      'polygonCapColor',
+      'polygonSideColor',
+      'polygonStrokeColor',
+      'polygonAltitude',
+      'polygonsTransitionDuration',
+      'pathsData',
+      'pathPoints',
+      'pathPointLat',
+      'pathPointLng',
+      'pathPointAlt',
+      'pathResolution',
+      'pathColor',
+      'pathStroke',
+      'pathDashLength',
+      'pathDashGap',
+      'pathDashInitialGap',
+      'pathDashAnimateTime',
+      'pathTransitionDuration',
+      'hexBinPointsData',
+      'hexBinPointLat',
+      'hexBinPointLng',
+      'hexBinPointWeight',
+      'hexBinResolution',
+      'hexMargin',
+      'hexTopColor',
+      'hexSideColor',
+      'hexAltitude',
+      'hexBinMerge',
+      'hexTransitionDuration',
+      'hexPolygonsData',
+      'hexPolygonGeoJsonGeometry',
+      'hexPolygonColor',
+      'hexPolygonAltitude',
+      'hexPolygonResolution',
+      'hexPolygonMargin',
+      'hexPolygonsTransitionDuration',
+      'labelsData',
+      'labelLat',
+      'labelLng',
+      'labelAltitude',
+      'labelRotation',
+      'labelText',
+      'labelSize',
+      'labelTypeFace',
+      'labelColor',
+      'labelResolution',
+      'labelIncludeDot',
+      'labelDotRadius',
+      'labelDotOrientation',
+      'labelsTransitionDuration',
+      'customLayerData',
+      'customThreeObject',
+      'customThreeObjectUpdate'
+    ];
+
+    const newProps = Object.assign({},
+      ...Object.entries(state)
+        .filter(([prop, val]) => changedProps.hasOwnProperty(prop) && passThroughProps.indexOf(prop) != -1 && val !== undefined && val !== null)
+        .map(([key, val]) => ({ [key]: val }))
+    );
+
+    state.globe.setAttribute('globe', newProps);
+  }
+});
