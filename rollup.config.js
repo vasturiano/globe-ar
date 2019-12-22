c81dfa5796e79fb929d1a2dae4e7fc661f054012
vasco <vastur@gmail.com>
2019-12-22 00:05:21 -0800

    Setup package

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonJs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import { name, homepage, version } from './package.json';
+
+export default {
+  external: ['three'],
+  input: 'src/index.js',
+  output: [
+    {
+      globals: { three: 'THREE' }, // a-frame exposes three as global
+      format: 'umd',
+      name: 'GlobeAR',
+      file: `dist/${name}.js`,
+      sourcemap: true,
+      banner: `// Version ${version} ${name} - ${homepage}`
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonJs(),
+    babel({ exclude: 'node_modules/**' })
+  ]
+};
