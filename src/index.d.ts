a9d389da4629aca87aec740182c4e2027aca742b
vasco <vastur@gmail.com>
2020-03-16 10:42:30 -0700

    Add TS declarations

---
+++ src/index.d.ts
@@ -0,0 +1,35 @@
+import { ThreeGlobeGeneric } from 'three-globe';
+
+export interface ConfigOptions {
+  markerAttrs?: object;
+}
+
+type hoverObj = {
+  type: string;
+  data: object;
+}
+
+export interface GlobeGenericInstance<ChainableInstance> extends ThreeGlobeGeneric<ChainableInstance> {
+  (element: HTMLElement): ChainableInstance;
+  resetProps(): ChainableInstance;
+  _destructor(): void;
+
+  // Container layout
+  width(): number;
+  width(width: number): ChainableInstance;
+  height(): number;
+  height(height: number): ChainableInstance;
+  yOffset(): number;
+  yOffset(numMarkers: number): ChainableInstance;
+  globeScale(): number;
+  globeScale(globeRadii: number): ChainableInstance;
+
+  // Interaction events
+  onCenterHover(callback: (obj: hoverObj | null, prevObj: hoverObj | null) => void): ChainableInstance;
+}
+
+export type GlobeInstance = GlobeGenericInstance<GlobeInstance>;
+
+declare function Globe(configOptions?: ConfigOptions): GlobeInstance;
+
+export default Globe;
